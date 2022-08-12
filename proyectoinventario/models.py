799d094623597372bd709a2fa4ace4465f081e61
lupitaesp <lupitaesp.com>
2022-08-12 11:09:16 -0500

    ASIGNA IP

--- proyectoinventario/models.py
+++ proyectoinventario/models.py
@@ -30,6 +30,6 @@ class Clientes(models.Model):
 
 class Ip(models.Model):
     id=models.AutoField(primary_key=True)
-    ip=models.CharField(max_length=12, unique= False)
+    ip=models.CharField(max_length=12, unique= True)
     equipo= models.CharField(max_length=20)
     cliente=models.CharField(max_length=30)
