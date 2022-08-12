b191f971795ec63b3f072e121270376df80f1e42
lupitaesp <lupitaesp.com>
2022-08-12 12:20:59 -0500

    ADMINISTRACION

--- proyectoinventario/admin.py
+++ proyectoinventario/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Assets, Clientes
+from .models import Assets, Clientes, Ip
 
 
 admin.site.register(Assets)
 admin.site.register(Clientes)
+admin.site.register(Ip)
+
 
 
 # Register your models here.
